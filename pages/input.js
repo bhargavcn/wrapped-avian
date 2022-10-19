72208098de32d7393c7bd1d4a667d4491002c959
Your Name <you@example.com>
2022-10-19 20:19:21 +0530

    fixed errors

--- pages/input.js
+++ pages/input.js
@@ -1,6 +1,10 @@
-function myFunction() {
+window.onload = function () {
+  document.getElementById("avn").value = document.getElementById("avn").value;
+  document.getElementById("wavn").value = document.getElementsByName("wavn")[0].value;
+  var src = document.getElementById("avn"),
+    dst = document.getElementById("wavn");
+  src.addEventListener("input", function () {
+    dst.value = src.value;
+  });
 
-    var x = document.getElementById("avn").value;
-    document.getElementById("wavn").placeholder = "junaid";
-    }
-myFunction();
+}
